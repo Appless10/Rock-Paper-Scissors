2a69b48a9a211875efa5cccc703ba3a16f3b0251
SageSpikes <boppanajishnu@gmail.com>
2024-07-21 13:31:35 -0400

    Added computer choice function in js

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice(){
+    let compChoice = Math.floor(Math.random() * 3);
+
+    if (compChoice == 0) {
+        return "Rock";
+    } else if (compChoice == 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+console.log(getComputerChoice());
