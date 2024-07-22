60663c815c58872dc03c1229ec54400b1b8d76bf
SageSpikes <boppanajishnu@gmail.com>
2024-07-22 14:44:58 -0400

    Added playRound function

--- script.js
+++ script.js
@@ -5,16 +5,45 @@ function getComputerChoice(){
     let compChoice = Math.floor(Math.random() * 3);
 
     if (compChoice == 0) {
-        return "Rock";
+        return "rock";
     } else if (compChoice == 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function getHumanChoice(){
-    let humanChoice = prompt("Enter either \'Rock\', \'Paper\', or \'Scissors\': ");
-    return humanChoice;
+    let humnChoice = prompt("Enter either \'Rock\', \'Paper\', or \'Scissors\': ");
+    return humnChoice;
 }
 
+function playRound(humanChoice, computerChoice){
+    let humanChoiceLower = humanChoice.toLowerCase();
+    if (humanChoiceLower == "rock" && computerChoice == "paper"){
+        computerScore ++;
+        return "You lose! Paper beats rock";
+    } else if (humanChoiceLower == "rock" && computerChoice == "scissors") {
+        humanScore ++;
+        return "You win! Rock beats scissors";
+    } else if (humanChoiceLower == "scissors" && computerChoice == "rock") {
+        computerScore ++;
+        return "You lose! Rock beats scissors";
+    } else if (humanChoiceLower == "scissors" && computerChoice == "paper") {
+        humanScore ++;
+        return "You win! Scissors beats paper";
+    } else if (humanChoiceLower == "paper" && computerChoice == "rock") {
+        humanScore ++;
+        return "You win! Paper beats rock";
+    } else if (humanChoiceLower == "paper" && computerChoice == "scissors") {
+        computerScore ++;
+        return "You lose! Scissors beats rock";
+    } else if (humanChoiceLower == computerChoice) {
+        return "Its a tie! You both selected the same option";
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
