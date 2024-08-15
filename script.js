35df7176c09955427b63ec1b97c3fef5c6c1ab93
SageSpikes <boppanajishnu@gmail.com>
2024-08-15 18:11:37 -0400

    Got basic score working

--- script.js
+++ script.js
@@ -10,19 +10,9 @@ function playGame(){
     let humanScore = 0;
 
 
-    function getComputerChoice(){
-        let compChoice = Math.floor(Math.random() * 3);
-    
-        if (compChoice == 0) {
-            return "rock";
-        } else if (compChoice == 1) {
-            return "paper";
-        } else {
-            return "scissors";
-        }
-    }
 
-    let computerSelection = getComputerChoice(); 
+    // Have to call getComputerChoice each time button is clicked
+
     
     
 
@@ -32,60 +22,101 @@ function playGame(){
         rock.addEventListener("click", () => {
              humnChoice = "Rock";
              //change to call function inside function
-             playRound(humnChoice, computerSelection);
+             playRound(humnChoice);
+             endGame();
         });
 
         paper.addEventListener("click", () => {
              humnChoice = "Paper";
-             playRound(humnChoice, computerSelection);
+             playRound(humnChoice);
+             endGame();
         });
 
         scissors.addEventListener("click", () => {
              humnChoice = "Scissors";
-             playRound(humnChoice, computerSelection);
+             playRound(humnChoice);
+             endGame();
         });
     
 
+        
+    
+        let computerSelection;
 
-    function playRound(humanChoice, computerChoice){
+
+    function playRound(humanChoice){
+
+        function getComputerChoice(){ 
+            let compChoice = Math.floor(Math.random() * 3);
+        
+            if (compChoice == 0) {
+                return "rock";
+            } else if (compChoice == 1) {
+                return "paper";
+            } else {
+                return "scissors";
+            }
+        }
+
+        let computerChoice = getComputerChoice();
         let humanChoiceLower = humanChoice.toLowerCase();
         if (humanChoiceLower == "rock" && computerChoice == "paper"){
             computerScore ++;
+            console.log("Player chose: " + humnChoice);
+            console.log("Computer chose: " + computerChoice);
             console.log("You lose! Paper beats rock");
         } else if (humanChoiceLower == "rock" && computerChoice == "scissors") {
             humanScore ++;
+            console.log("Player chose: " + humnChoice);
+            console.log("Computer chose: " + computerChoice);
             console.log("You win! Rock beats scissors");
         } else if (humanChoiceLower == "scissors" && computerChoice == "rock") {
             computerScore ++;
+            console.log("Player chose: " + humnChoice);
+            console.log("Computer chose: " + computerChoice);
             console.log("You lose! Rock beats scissors");
         } else if (humanChoiceLower == "scissors" && computerChoice == "paper") {
             humanScore ++;
+            console.log("Player chose: " + humnChoice);
+            console.log("Computer chose: " + computerChoice);
             console.log("You win! Scissors beats paper");
         } else if (humanChoiceLower == "paper" && computerChoice == "rock") {
             humanScore ++;
+            console.log("Player chose: " + humnChoice);
+            console.log("Computer chose: " + computerChoice);
             console.log("You win! Paper beats rock");
         } else if (humanChoiceLower == "paper" && computerChoice == "scissors") {
             computerScore ++;
+            console.log("Player chose: " + humnChoice);
+            console.log("Computer chose: " + computerChoice);
             console.log("You lose! Scissors beats paper");
         } else if (humanChoiceLower == computerChoice) {
+            console.log("Player chose: " + humnChoice);
+            console.log("Computer chose: " + computerChoice);
             console.log("Its a tie! You both selected the same option");
         }
     }
 
+    function endGame(){
     
-     console.log("Player chose: " + humnChoice);
-     console.log("Computer chose: " + computerSelection);
+     console.log("Computer ended with " + computerScore + " points");
+     console.log("Human ended with " + humanScore + " points");
     
-
-    console.log("Computer ended with " + computerScore + " points");
-    console.log("Human ended with " + humanScore + " points");
-    
-    if (computerScore > humanScore) {
-        console.log("Computer won, sorry :(");
-    } else if (humanScore > computerScore) {
-        console.log("Human won, congrats :)");
-    } else if (humanScore == computerScore) {
-        console.log("It was a tie, both human and computer had the same amount of points");
+     if (computerScore == 5 || humanScore == 5) {
+     if (computerScore > humanScore) {
+         console.log("Computer won, sorry :(");
+         humanScore = 0;
+         computerScore = 0;
+     } else if (humanScore > computerScore) {
+         console.log("Human won, congrats :)");
+         humanScore = 0;
+         computerScore = 0;
+     } else if (humanScore == computerScore) {
+         console.log("It was a tie, both human and computer had the same amount of points");
+         humanScore = 0;
+         computerScore = 0;
+     }
+     }
     }
 
 
